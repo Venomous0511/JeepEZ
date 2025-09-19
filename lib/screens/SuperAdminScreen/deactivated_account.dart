9c70d2175a8814fb758aa773701a5a8f6e808496
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2025-09-20 01:06:30 +0800

    Release v0.1.0

--- lib/screens/SuperAdminScreen/deactivated_account.dart
+++ lib/screens/SuperAdminScreen/deactivated_account.dart
@@ -100,121 +100,6 @@ class _DeactivatedAccountScreenState extends State<DeactivatedAccountScreen> {
             );
           }
       )
-
-
-      // LayoutBuilder(
-      //   builder: (context, constraints) {
-      //     return SingleChildScrollView(
-      //       scrollDirection: Axis.vertical,
-      //       child: SingleChildScrollView(
-      //         scrollDirection: Axis.horizontal,
-      //         child: Container(
-      //           constraints: const BoxConstraints(minWidth: 600),
-      //           padding: const EdgeInsets.all(16),
-      //           child: Table(
-      //             columnWidths: const {
-      //               0: FlexColumnWidth(2), // ID Number
-      //               1: FlexColumnWidth(2), // Status
-      //               2: FlexColumnWidth(2), // Name
-      //               3: FlexColumnWidth(3), // Email
-      //             },
-      //             border: TableBorder.all(color: Colors.grey.shade300),
-      //             children: [
-      //               // Header Row
-      //               TableRow(
-      //                 decoration: BoxDecoration(color: Colors.blue[50]),
-      //                 children: const [
-      //                   Padding(
-      //                     padding: EdgeInsets.all(12.0),
-      //                     child: Text(
-      //                       "ID Number",
-      //                       style: TextStyle(fontWeight: FontWeight.bold),
-      //                       textAlign: TextAlign.center,
-      //                     ),
-      //                   ),
-      //                   Padding(
-      //                     padding: EdgeInsets.all(12.0),
-      //                     child: Text(
-      //                       "Status",
-      //                       style: TextStyle(fontWeight: FontWeight.bold),
-      //                       textAlign: TextAlign.center,
-      //                     ),
-      //                   ),
-      //                   Padding(
-      //                     padding: EdgeInsets.all(12.0),
-      //                     child: Text(
-      //                       "Name",
-      //                       style: TextStyle(fontWeight: FontWeight.bold),
-      //                       textAlign: TextAlign.center,
-      //                     ),
-      //                   ),
-      //                   Padding(
-      //                     padding: EdgeInsets.all(12.0),
-      //                     child: Text(
-      //                       "Email",
-      //                       style: TextStyle(fontWeight: FontWeight.bold),
-      //                       textAlign: TextAlign.center,
-      //                     ),
-      //                   ),
-      //                 ],
-      //               ),
-      //               // Data Rows
-      //               ...attendanceData.map((record) {
-      //                 int index = attendanceData.indexOf(record);
-      //                 return TableRow(
-      //                   decoration: BoxDecoration(
-      //                     color: index % 2 == 0
-      //                         ? Colors.white
-      //                         : Colors.grey[100],
-      //                   ),
-      //                   children: [
-      //                     Padding(
-      //                       padding: const EdgeInsets.all(12.0),
-      //                       child: Text(
-      //                         record['ID number'] ?? '',
-      //                         textAlign: TextAlign.center,
-      //                         style: const TextStyle(color: Colors.black87),
-      //                       ),
-      //                     ),
-      //                     Padding(
-      //                       padding: const EdgeInsets.all(12.0),
-      //                       child: Text(
-      //                         record['Status'] ?? '',
-      //                         textAlign: TextAlign.center,
-      //                         style: const TextStyle(
-      //                           color: Colors.green,
-      //                           fontWeight: FontWeight.w500,
-      //                         ),
-      //                       ),
-      //                     ),
-      //                     Padding(
-      //                       padding: const EdgeInsets.all(12.0),
-      //                       child: Text(
-      //                         record['Name'] ?? '',
-      //                         textAlign: TextAlign.center,
-      //                         style: const TextStyle(color: Colors.black87),
-      //                       ),
-      //                     ),
-      //                     Padding(
-      //                       padding: const EdgeInsets.all(12.0),
-      //                       child: Text(
-      //                         record['Email'] ?? '',
-      //                         maxLines: 1,
-      //                         overflow: TextOverflow.ellipsis,
-      //                         textAlign: TextAlign.center,
-      //                         style: const TextStyle(color: Colors.black87),
-      //                       ),
-      //                     ),
-      //                   ],
-      //                 );
-      //               }),
-      //             ],
-      //           ),
-      //         ),
-      //       ),
-      //     );
-      //   },
-      // ),
     );
   }
   /// ------------ REACTIVATE USER FUNCTION ------------
@@ -222,8 +107,6 @@ class _DeactivatedAccountScreenState extends State<DeactivatedAccountScreen> {
     final emailCtrl = TextEditingController();
     final passCtrl = TextEditingController();
     final nameCtrl = TextEditingController();
-    String role = "conductor";
-    final employeeIdCtrl = TextEditingController();
 
     final confirmed = await showDialog<bool>(
       context: context,
