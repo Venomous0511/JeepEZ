dab474cdb825e0f1f91534ddfe2b4980139fe1f4
Jeriellog <jerielcelis195@gmail.com>
2025-09-27 02:24:18 +0800

    Fix in the sdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.jeepez"
-    compileSdk = 34            
+    compileSdk = 35         
     ndkVersion = "29.0.13846066"          
 
     compileOptions {
