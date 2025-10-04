d2e8e395708947f9406f24df081fba6d7819b2a6
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2025-10-04 11:07:21 +0800

    Release v0.5.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,7 +57,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
+//            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             isShrinkResources = false
         }
